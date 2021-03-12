0c4fecdc4835fffc733f473975b2145933198772
nevercase <1024769485@qq.com>
2021-03-12 15:13:11 +0800

    date-agent-client add cmd timeout

--- time.go
+++ time.go
@@ -3,12 +3,25 @@ package date_agent
 import (
 	"context"
 	"k8s.io/klog/v2"
+	"os"
 	"os/exec"
+	"strconv"
 	"time"
 )
 
+const CommandTimeoutEnv = "DATE_AGENT_CMD_TIMEOUT"
+const CommandTimeout = 15
+
 func Exec(suffix []string) (out string, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	t := CommandTimeout
+	if x := os.Getenv(CommandTimeoutEnv); x != "" {
+		if t, err = strconv.Atoi(x); err != nil {
+			klog.V(2).Info(err)
+		} else {
+			t = CommandTimeout
+		}
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(t))
 	var res []byte
 	commands := []string{"-c"}
 	commands = append(commands, suffix...)
